cd6b69f5141505bd921c2686753cf4d222ed6c2b
Aaryan410 <jadavaaryan280@gmail.com>
2026-09-25 23:07:37 +0530

    implemented a code to catch specific keywords required and create a DOM to store them

--- content/content.js
+++ content/content.js
@@ -4,9 +4,19 @@ for (const heading of headings) {
     const text = heading.innerText;
 
     if (text.includes("M5")) {
+        const product = heading.querySelector(".product-tile-headline");
         const hardware = heading.querySelector(".product-tile-subheading");
 
-        console.log("Product:", heading.querySelector(".product-tile-headline")?.innerText);
-        console.log("Hardware:", hardware?.innerText);
+        const cleanedHardware = hardware?.innerText?.replace("chip", "").trim();
+
+        console.log("RAW HARDWARE:", JSON.stringify(hardware?.innerText));
+        console.log("CLEANED HARDWARE:", JSON.stringify(cleanedHardware));
+
+        const productData = {
+            product: product?.innerText,
+            hardware: cleanedHardware
+        };
+
+        console.log(productData);
     }
 }
